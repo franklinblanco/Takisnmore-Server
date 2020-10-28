1f6d3141f7f3cd98b36bfb5edb284b66ab575cf4
Franklin Blanco <franklinblanco62@gmail.com>
2020-10-28 11:11:18 -0400

    Added a Shop object to facilitate storing sql data in the cachemanager.

---
+++ Server/Shop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    public class Shop
+    {
+        public string id;
+        public string itemname;
+        public string owneraccountid;
+        public string description;
+        public string logoid;
+        public string phonenumber;
+    }
+}
